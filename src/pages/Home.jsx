bb765d6926ebb526007d9850f60b994d1bde3340
SehwanChang <kdwkdwkd@naver.com>
2024-06-04 19:56:05 +0900

    [feat] test수정

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SortToggle from "../components/home/SortToggle";
+import PostListSection from "../components/home/PostListSection";
+
+function Home() {
+  return (
+    <>
+      <SortToggle />
+      <PostListSection />
+      <div>test</div>
+    </>
+  );
+}
+
+export default Home;
